46b86c7371a4332e2a186ebbae7986d588f14e9d
Christoph Langer <c.langer@orbitgrowth.de>
2023-06-29 23:13:25 +0200

    feat: implement basic Internationalization

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,6 +1,11 @@
 import { SignatureContextProvider } from '@/lib/context/signature-context';
 import { Inter } from 'next/font/google';
-import '../styles/globals.css';
+import { i18n } from '../../../i18n-config';
+import '../../styles/globals.css';
+
+export async function generateStaticParams() {
+  return i18n.locales.map((locale) => ({ lang: locale }));
+}
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,11 +16,13 @@ export const metadata = {
 
 export default function RootLayout({
   children,
+  params,
 }: {
   children: React.ReactNode;
+  params: { lang: string };
 }) {
   return (
-    <html lang="en">
+    <html lang={params.lang}>
       <body className={inter.className}>
         <main>
           <SignatureContextProvider>
---
+++ src/app/[lang]/page.tsx
@@ -0,0 +1,28 @@
+import Counter from '@/components/counter';
+import LocaleSwitcher from '@/components/locale-switcher';
+import { getDictionary } from '@/lib/get-dictionary';
+import { Locale } from '../../../i18n-config';
+
+type PageProps = {
+  params: {
+    lang: Locale;
+  };
+};
+
+export default async function Home({ params: { lang } }: PageProps) {
+  const dictionary = await getDictionary(lang);
+
+  return (
+    <>
+      <div>
+        <LocaleSwitcher />
+        <p>Current locale: {lang}</p>
+        <p>
+          This text is rendered on the server:{' '}
+          {dictionary['server-component'].welcome}
+        </p>
+        <Counter dictionary={dictionary.counter} />
+      </div>
+    </>
+  );
+}
